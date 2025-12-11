589fbf86203c03186002f1873fafec8874ed8d56
June Leoman Lapera <leomanlapera@gmail.com>
2025-12-11 22:02:50 +0800

    player movement

---
+++ player/player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const GRAVITY: float = 1000.0
+const SPEED: float = 300.0
+
+enum State { Idle, Run }
+
+var current_state
+
+
+func _ready() -> void:
+	current_state = State.Idle
+
+	
+func _process(delta: float) -> void:
+	player_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+
+	move_and_slide()
+
+	player_animations()
+
+
+func player_falling(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+
+
+func player_idle(delta: float) -> void:
+	if is_on_floor():
+		current_state = State.Idle
+		print("State: ", State.keys()[current_state])
+
+
+func player_run(delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction != 0:
+		current_state = State.Run
+		print("State: ", State.keys()[current_state])
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+	
+	
+func player_animations():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("run")
+	
+	
