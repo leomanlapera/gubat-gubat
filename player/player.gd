96dcf02d03b7295a0f378d34a9cded6c89b2a32f
june <leomanlapera@gmail.com>
2025-12-12 21:05:16 +0800

    player
    jump script and animation

--- player/player.gd
+++ player/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 
 const GRAVITY: float = 1000.0
 const SPEED: float = 300.0
+const JUMP: float = -300.0
+const JUMP_HORIZONTAL: float = 100.0
 
-enum State { Idle, Run }
+enum State { Idle, Run, Jump }
 
 var current_state
 
@@ -13,15 +15,15 @@ var current_state
 func _ready() -> void:
 	current_state = State.Idle
 
-	
+
 func _process(delta: float) -> void:
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
-
+	player_jump(delta)
 	move_and_slide()
-
 	player_animations()
+	print("State: ", State.keys()[current_state])
 
 
 func player_falling(delta: float) -> void:
@@ -32,10 +34,12 @@ func player_falling(delta: float) -> void:
 func player_idle(delta: float) -> void:
 	if is_on_floor():
 		current_state = State.Idle
-		print("State: ", State.keys()[current_state])
 
 
 func player_run(delta: float) -> void:
+	if !is_on_floor():
+		return
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction:
@@ -45,14 +49,23 @@ func player_run(delta: float) -> void:
 
 	if direction != 0:
 		current_state = State.Run
-		print("State: ", State.keys()[current_state])
 		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+
+func player_jump(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP
+		current_state = State.Jump
 	
-	
+	if !is_on_floor() and current_state == State.Jump:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * JUMP_HORIZONTAL * delta
+
+
 func player_animations():
 	if current_state == State.Idle:
 		animated_sprite_2d.play("idle")
 	elif current_state == State.Run:
 		animated_sprite_2d.play("run")
-	
-	
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("jump")
